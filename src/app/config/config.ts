d7fd3c4ada39f45a57ab3bbf07e2dabe7bfad44f
Daniel Rodriguez <Daniel@Daniels-MacBook-Pro.local>
2018-03-31 14:49:01 -0400

    Various tweaks. Added config file.

---
+++ src/app/config/config.ts
@@ -0,0 +1,7 @@
+/*
+ *  Essentially a config file, but read in as a JSON variable to any component
+ *  that imports it.
+ */
+export var config = {
+  apiHost: "http://localhost:8080"
+}
--- src/app/service/rest/rest.service.ts
+++ src/app/service/rest/rest.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { config } from '../../config/config';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
@@ -9,32 +10,32 @@ export class RestService {
   constructor(private http: Http) {}
 
   getArtists() {
-    return this.http.get("http://localhost:8080/artists")
+    return this.http.get(config.apiHost + "/artists")
     .map((response: Response) => response.json());
   }
 
   getArtist(artistId) {
-    return this.http.get("http://localhost:8080/artists/" + artistId)
+    return this.http.get(config.apiHost + "/artists/" + artistId)
     .map((response: Response) => response.json());
   }
 
   getAlbums(artistId) {
-    return this.http.get("http://localhost:8080/artists/" + artistId + "/albums")
+    return this.http.get(config.apiHost + "/artists/" + artistId + "/albums")
     .map((response: Response) => response.json());
   }
 
   getAlbum(artistId, albumId) {
-    return this.http.get("http://localhost:8080/artists/" + artistId + "/albums/" + albumId)
+    return this.http.get(config.apiHost + "/artists/" + artistId + "/albums/" + albumId)
     .map((response: Response) => response.json());
   }
 
   getSongs(artistId, albumId) {
-    return this.http.get("http://localhost:8080/artists/" + artistId + "/albums/" + albumId + "/songs")
+    return this.http.get(config.apiHost + "/artists/" + artistId + "/albums/" + albumId + "/songs")
     .map((response: Response) => response.json());
   }
 
   getSong(artistId, albumId, songId) {
-    return this.http.get("http://localhost:8080/artists/" + artistId + "/albums/" + albumId + "/songs/" + songId)
+    return this.http.get(config.apiHost + "/artists/" + artistId + "/albums/" + albumId + "/songs/" + songId)
     .map((response: Response) => response.json());
   }
 
